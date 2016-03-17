d10bf426205ca97ba25f6f73eba7b812a593e2f9
monox334 <monox_nino@hotmail.com>
2016-03-17 12:31:43 -0300

    Segundo tarea terminada!

---
+++ concatenacion_mayusculas.py
@@ -0,0 +1,14 @@
+def imprimir(x)
+    return x.upper()
+
+
+if __name__ == "__main__":
+
+    x=input("Ingrese cualquier cosa sin incluir simbolos ni numeros: ")
+    print("Para terminar ingrese un espacio en blanco")
+    while (x!= " ")
+        y=input("Ingrese cualquier cosa sin incluir simbolos ni numeros: ")
+        x=x+y
+    x=x.upper()
+    print(x)
+
